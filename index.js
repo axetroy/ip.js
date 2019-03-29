492f22c45f3614472fe83ba58763f0722575a3b4
axetroy <troy450409405@gmail.com>
2019-03-29 11:19:55 +0800

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+// Creating new HTTP server.
+http
+  .createServer((req, res) => {
+    const ip =
+      (req.headers["x-forwarded-for"] || "").split(",").pop() ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(ip);
+  })
+  .listen(port, () => {
+    console.log("listen on port " + port);
+  });
