b98534289c6357a181cbc062892998bfe705c5d0
axetroy <troy450409405@gmail.com>
2019-03-29 17:17:51 +0800

    update

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const port = process.env.PORT || 3000;
 http
   .createServer((req, res) => {
     const ip =
-      (req.headers["x-forwarded-for"] || "").split(",").pop() ||
+      (req.headers["x-forwarded-for"] || "").split(",").shift() ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       req.connection.socket.remoteAddress;
+    console.log(`request by ${ip}`);
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end(ip);
   })
